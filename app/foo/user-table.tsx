175666d1715f6f18ebce1d6fe353e17b321e0ea4
guidoffm <guidoffm@users.noreply.github.com>
2024-05-12 22:43:40 +0200

    Basic sort working

--- app/foo/user-table.tsx
+++ app/foo/user-table.tsx
@@ -3,30 +3,51 @@
 import { User } from "./user-type";
 import UserRow from "./user-row";
 import { Key, useEffect, useState } from "react";
+type UserProps = 'city' | 'state' | 'personId' | 'personOrg';
 export default function UserTable({ data }: { data: User[] }) {
-    // introduce useEffect and useState
-    const [users, setUsers] = useState<any[]>([]);
-    useEffect(() => {
-        // fetch data
-        setUsers(data);
-    }, []);
 
-    const handleHeaderClick = (columnName: string) => () => {
-        console.log(`${columnName} header clicked`);
+    const [users, setUsers] = useState<User[]>([]);
+    const [dataMyTable, setdataMyTable] = useState<User[]>([]);
+    const [sortColumn, setSortColumn] = useState<UserProps>('city');
+    
+    useEffect(() => {
+        setUsers(data);
+    }, [data]);
+    
+    useEffect(() => {
+        const sortedUsers = getsortedUsers();
+        setdataMyTable(sortedUsers as never[]);
+    }, [users, sortColumn]);
+
+    function handleHeaderClick(columnName: UserProps) {
+        // console.log(`${columnName} header clicked`);
+        setSortColumn(columnName);
     };
-    // console.log(users);
+
+    function getsortedUsers() {
+        return [...users].sort((a, b) => {
+            if (a[sortColumn] < b[sortColumn]) {
+                return -1;
+            }
+            if (a[sortColumn] > b[sortColumn]) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
     return (
         <table>
             <thead>
                 <tr>
-                    <th scope="col" onClick={handleHeaderClick('city')}>City</th>
-                    <th scope="col" onClick={handleHeaderClick('state')}>State</th>
-                    <th scope="col" onClick={handleHeaderClick('personId')}>Person ID</th>
-                    <th scope="col" onClick={handleHeaderClick('personOrg')}>Person Org</th>
+                    <th scope="col" onClick={()=>handleHeaderClick('city')}>City</th>
+                    <th scope="col" onClick={()=>handleHeaderClick('state')}>State</th>
+                    <th scope="col" onClick={()=>handleHeaderClick('personId')}>Person ID</th>
+                    <th scope="col" onClick={()=>handleHeaderClick('personOrg')}>Person Org</th>
                 </tr >
             </thead >
             <tbody>
-                {users && (users as any).map((user: any, index: Key | null | undefined) => (
+                {(dataMyTable as User[]).map((user: User, index: Key) => (
                     <UserRow key={index} data={user}></UserRow>
                 ))}
             </tbody>
