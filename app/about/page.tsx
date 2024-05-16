3554bc869f20c1d147d8cb55a6a3ad50ef6795e9
guidoffm <guidoffm@users.noreply.github.com>
2024-05-16 14:51:46 +0200

    Update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ export default async function Page() {
 
     return (
         <main className="flex min-h-screen flex-col items-left justify-between p-24">
-            <div>
+            <div className="z-10 w-full max-w-5xl items-center justify-between text-md lg:flex">
                 Über uns
             </div>
         </main>
