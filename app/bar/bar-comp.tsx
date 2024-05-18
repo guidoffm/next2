f2f7453fa4b0f4e0664bda3515ea5fc5b85afb87
guidoffm <guidoffm@users.noreply.github.com>
2024-05-18 19:29:02 +0200

    fix bar comp

--- app/bar/bar-comp.tsx
+++ app/bar/bar-comp.tsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import BarFunc from "./bar-func";
 
 export default function BarComp() {
 
     const [barCompContent, setBarCompContent] = useState('');
 
-    const fetchBarFunc = async function () {
+    const fetchBarFunc = useCallback(async function () {
         const barFuncResult = await BarFunc();
         setBarCompContent(barFuncResult);
-    }
+    }, []);
 
     useEffect(() => {
         fetchBarFunc();
